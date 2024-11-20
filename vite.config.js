9984284ca769d4cb9433981c6e34a1aca16dbaba
escotilha <p@nove.co>
2024-11-20 11:07:07 -0300

    Update deployment configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/MNACalculator/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    chunkSizeWarningLimit: 1000,
+  },
 })
